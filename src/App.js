8eb7354811d75ca0bb62ec1ff284b0a9b562275e
Ansh1vohra <anshvohra1@gmail.com>
2025-02-07 22:45:30 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header.tsx';
+
+function App() {
+  return (
+    <div className='mainContent h-screen bg-[#fdfdf9]'>
+        <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <>
+            <header className='p-4 bg-violet-200 flex justify-between'>
+                <h1 className='text-[1.5rem] font-semibold'>Edublog</h1>
+                <div>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </header>
+        </>
+    )
+}
