bb28ed7fb3de2e3df91a093cde9b620d16b6fdaa
Ansh1vohra <anshvohra1@gmail.com>
2025-03-30 00:25:43 +0530

    updated Loading

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -90,7 +90,7 @@ export default function Home() {
 
             {loading ? (
                 <div className='h-80'>
-                    <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status"></div>
+                    <div className="m-6 loader"></div>
                 </div>
             ) : error ? (
                 <p className="h-80 text-red-500">{error}</p>
